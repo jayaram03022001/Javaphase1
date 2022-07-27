78fe5cc86b3cd7ed1915e39125cd6dbc62479086
jayaram03022001 <109974281+jayaram03022001@users.noreply.github.com>
2022-07-27 19:16:06 +0530

    Add files via upload

---
+++ InnerDemo.java
@@ -0,0 +1,11 @@
+package com.Ram;
+import com.Ram.*;
+public class InnerDemo {
+	public static void main(String[]args)
+	{
+		Student s=new Student();
+		s.rollno=9;
+		System.out.print(s.rollno);
+	}
+
+}
