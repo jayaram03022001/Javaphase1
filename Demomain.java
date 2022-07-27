7f8d5754b1fe4e8e0ffa4f6d8d2a22e0db89fcea
jayaram03022001 <109974281+jayaram03022001@users.noreply.github.com>
2022-07-27 19:14:19 +0530

    Add files via upload

---
+++ Demomain.java
@@ -0,0 +1,13 @@
+package com.Ram;
+
+public class Demomain {
+	public static void main(String[] args)
+	{
+		Student s = new Student();
+		s.rollno=4;
+		s.marks=90;
+		System.out.println(s.rollno);
+		System.out.println(s.marks);
+	}
+
+}
