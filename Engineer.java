a9e2e23165dc2a1e38b5c3bd5c49908fa6245ea0
jayaram03022001 <109974281+jayaram03022001@users.noreply.github.com>
2022-07-27 19:15:14 +0530

    Add files via upload

---
+++ Engineer.java
@@ -0,0 +1,14 @@
+package com.Ram;
+import com.Ram.*;
+public class Engineer extends Student{
+       public void show()
+       {
+    	   marks=70;
+       }
+       public static void main(String[] args)
+       {
+    	   Engineer ob= new Engineer();
+    	   System.out.println(ob.marks);
+    	   
+       }
+}
